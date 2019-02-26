b4e58c7ca0f2b1c29bbb8d28fff7079dd1e60742
Jonelle Carless <jonellecarless@Jonelles-MBP.fios-router.home>
2019-02-26 13:13:31 -0500

    almost done, bug in petfinder search

--- README.md
+++ README.md
@@ -1,110 +1,15 @@
-# PETFINDER OR PETCHAT
+# PETFINDER
 
 # Requirements
-1. Must utilize one new library
-2. Must use at least two api's
-3. Write own API
-4. Route go into server
+1. Must utilize one new library: Materialize
+2. Must use at least two API's: Petfinder and Google Maps
+4. Route must go into server: Done
 
 # Description
-App will help you find a pet at a shelter/rescue or breeder then connect you so that you'll be able to find a pet close to you and connect to be able to video chat with the pet.
+App will help you find a pet in your area and lets you locate it on a map.
 
 ## -------------------- ##
 
-# Project Requirements
+# Heroku Link
+https://petfinderjonelle.herokuapp.com/petfinder.html
 
-This document outlines the base requirements for Project 2.
-
-### Requirements
-
-Your project must:
-
-* Use a Node and Express Web Server;
-
-* Be backed by a MySQL Database an ORM (not necessarily Sequelize);
-
-* Have both GET and POST routes for retrieving and adding new data;
-
-* Be deployed using Heroku (with Data);
-
-* Utilize at least one new library, package, or technology that we haven’t discussed;
-
-* Have a polished frontend / UI;
-
-* Have folder structure that meets MVC Paradigm;
-
-* Meet good quality coding standards (indentation, scoping, naming).
-
-* Must not expose sensitive API key information on the server, see [Protecting-API-Keys-In-Node.md](../../../10-nodejs/03-Supplemental/Protecting-API-Keys-In-Node.md)
-
-
-
-# SUGGESTIONS
-
-* Prior to implementing a function, use Mocha and Chai to write unit tests against it.
-
-* Run your tests prior to committing your code so you can be sure nothing breaks between commits.
-
-* **Non-Obligatory Bonus**: If you're confident and feel like you have some time to tinker, set up [Travis CI](https://travis-ci.org/). This will automatically run your tests whenever you or any of your team members makes a commit. 
-
-#### Directory structure
-
-All the recommended files and directories from the steps above should look like the following structure:
-
-```
-.
-├── config
-│   ├── connection.js
-│   └── orm.js
-│ 
-├── controllers
-│   └── burgers_controller.js
-│
-├── db
-│   ├── schema.sql
-│   └── seeds.sql
-│
-├── models
-│   └── burger.js
-│ 
-├── node_modules
-│ 
-├── package.json
-│
-├── public
-│   └── assets
-│       ├── css
-│       │   └── burger_style.css
-│       └── img
-│           └── burger.png
-│   
-│
-├── server.js
-│
-└── views
-    ├── index.handlebars
-    └── layouts
-        └── main.handlebars
-
-
-        # Week 16 - Project Suggestions
-
-### Overview
-
-Since projects are being worked on this week, you won't have a homework assignment, but a project is a perfect context in which to practice writing tests! 
-
-Incorporate a few of the below suggestions into your project. You'll write better code, and it will help you consolidate your testing habits.
-
-- - -
-
-### Testing Suggestions
-
-* Set up Karma so you can run your tests as you develop.
-
-* Prior to implementing a feature, use Nightmare to write a functional test for it.
-
-* Prior to implementing a function, use Mocha and Chai to write unit tests against it.
-
-* Run your tests prior to committing your code so you can be sure nothing breaks between commits.
-
-* **Non-Obligatory Bonus**: If you're confident and feel like you have some time to tinker, set up [Travis CI](https://travis-ci.org/). This will automatically run your tests whenever you or any of your team members makes a commit. 
--- app/public/petfinderapp.js
+++ app/public/petfinderapp.js
@@ -1,74 +1,124 @@
 // $(document).ready(function(){
 
 
-// Petfinder -- WORKING, just need to update
+// Petfinder -- working except for form
 var apiKey = '996d9ff2e72ad424c69b5b563ba1831c'; // assign our key to a variable, easier to read
 
 // the next line and function set up the button in our html to be clickable and reactive 
 document.addEventListener('DOMContentLoaded', bindButtons);
 
-function bindButtons(){
-	document.getElementById('submitZip').addEventListener('click', function(event){
-		event.preventDefault();
-		// this line gets the zip code from the form entry
-		var type = document.getElementById('type').value;
-		var age = document.getElementById('age').value;
-		var size = document.getElementById('size').value;
-		var zip = document.getElementById('zip').value;
-		var url = 'http://api.petfinder.com/pet.getRandom';
-		
-		// Within $.ajax{...} is where we fill out our query 
-		$.ajax({
-			url: url,
-			jsonp: "callback",
-			dataType: "jsonp",
-			data: {
-				key: apiKey,
-				animal: type,
-				'location': zip,
-				output: 'basic',
-				format: 'json',
-				'age': age,
-				'size': size
-			},
-			// Here is where we handle the response we got back from Petfinder
-			success: function(response) {
-				console.log(response); // debugging
-				var petName = response.petfinder.pet.name.$t;
-				var img = response.petfinder.pet.media.photos.photo[0].$t;
-				var id = response.petfinder.pet.id.$t;
-				var petType = response.petfinder.pet.animal.$t;
-				var petAge = response.petfinder.pet.age.$t;
-				var petSize = response.petfinder.pet.size.$t;
+function bindButtons() {
+ document.getElementById('submit').addEventListener('click', function (event) {
+  event.preventDefault();
+  // this line gets the information from the form entry
+  var type = document.getElementById("type").value("");
+  console.log(type);
+  var age = document.getElementById('age').value;
+  var size = document.getElementById('size').value;
+  var zip = document.getElementById('zip').value;
+  var url = 'http://api.petfinder.com/pet.getRandom';
 
-				var newName = document.createElement('a');
-				var newDiv = document.createElement('div');
-				var newType = document.createElement('a');
-				var newAge = document.createElement('a');
-				var newSize = document.createElement('a');
+  // Within $.ajax{...} is where we fill out our query 
+  $.ajax({
+   url: url,
+   jsonp: "callback",
+   dataType: "jsonp",
+   data: {
+    key: apiKey,
+    animal: type,
+    "location": zip,
+    output: 'full',
+    format: 'json',
+    'age': age,
+    "size": size
+   },
+   // Here is where we handle the response we got back from Petfinder
+   success: function (response) {
+    console.log(response); // debugging
+    var petName = response.petfinder.pet.name.$t;
+    var img = response.petfinder.pet.media.photos.photo[0].$t;
+    var id = response.petfinder.pet.id.$t;
+    var petType = response.petfinder.pet.animal.$t;
+    console.log(petType);
+    var petAge = response.petfinder.pet.age.$t;
+    var petSize = response.petfinder.pet.size.$t;
+    var location = response.petfinder.pet.contact.zip.$t;
+    console.log(location);
+    var petDesc = response.petfinder.pet.description.$t;
 
-				newName.textContent = petName;
-				newName.href = 'https://www.petfinder.com/petdetail/' + id;
-				newType.textContent = petType;
-				newAge.textContent = petAge;
-				newSize.textContent = petSize;
+    // Creating elements for each response
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute("class", "row")
 
-				var newImg = document.createElement('img');
-				newImg.src = img;
-				
-				var list = document.createElement("div");
-				list.setAttribute("id", "List");
-				document.body.appendChild(list);
+    var newCol = document.createElement('div');
+    newCol.setAttribute("class", "col s12 m12");
 
-				newDiv.appendChild(newName);
-				list.appendChild(newDiv);
-				list.appendChild(newImg);
-				list.appendChild(newType);
-				list.appendChild(newAge);
-				list.appendChild(newSize);
-			}
-		});
-		})
+
+    // Creating new card div with pet info
+    var newCard = document.createElement("div");
+    newCard.setAttribute("class", "card flex-item");
+
+    // Image variable
+    var newHeader = document.createElement('div');
+    newHeader.setAttribute("class", "card-image");
+
+    var newImg = document.createElement("img");
+    newImg.src = img;
+
+    // Pet Name
+    var newName = document.createElement('span');
+    newName.setAttribute("class", "card-title");
+    newName.textContent = petName;
+
+    // Pet type
+    var newType = document.createElement('div');
+    newType.textContent = petType;
+
+    // Pet age
+    var newAge = document.createElement("div");
+    newAge.textContent = petAge;
+
+    // Pet size
+    var newSize = document.createElement("div");
+    newSize.textContent = petSize;
+
+    // Pet location
+    var newLoc = document.createElement("div");
+    newLoc.textContent = location;
+
+    // Pet description
+    var newDesc = document.createElement("div");
+    newDesc.setAttribute("class", "card-content");
+    newDesc.textContent = petDesc;
+
+    var newAction = document.createElement("div");
+    newAction.setAttribute("class", "card-action");
+    var link = document.createElement("a");
+    link.textContent = "More Info Here"
+    link.href = 'https://www.petfinder.com/petdetail/' + id;
+
+
+
+
+
+    // Adding new info to body
+    document.body.appendChild(newDiv);
+
+    newDiv.appendChild(newCol);
+    newCol.appendChild(newCard)
+    newCard.appendChild(newHeader);
+    newHeader.appendChild(newImg);
+    newHeader.appendChild(newName);
+    newCard.appendChild(newType);
+    newCard.appendChild(newAge);
+    newCard.appendChild(newSize);
+    newCard.appendChild(newLoc)
+    newCard.appendChild(newDesc);
+    newCard.appendChild(newAction);
+    newAction.appendChild(link);
+   }
+  });
+ })
 
 }
 // });
